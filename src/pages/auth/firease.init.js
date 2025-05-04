855b5e0fb8c4928c3de43eb35213d1bf73f0ecfd
Mohamad Habib Ullah <mohammadhabibullah937@gmail.com>
2025-05-04 23:52:54 +0600

    add

--- src/pages/auth/firease.init.js
+++ src/pages/auth/firease.init.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDK you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -13,5 +13,5 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId
 };
 
-// Initialized Firebase
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
