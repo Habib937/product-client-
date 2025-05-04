18529e6660d826b5b468d6a23c6f2fc0c315b364
Mohamad Habib Ullah <mohammadhabibullah937@gmail.com>
2025-05-05 00:02:38 +0600

    add

--- src/pages/Users/Home.jsx
+++ src/pages/Users/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './Navbar';
-// import Banner from '../../components/Banner';
 import Featured from '../../components/Featured';
 import Slider from '../../components/Slider';
 import Tranding from './Tranding';
