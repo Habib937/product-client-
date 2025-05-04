e44a07fb93af6600a05ede37dd2b1f891ed4253b
Mohamad Habib Ullah <mohammadhabibullah937@gmail.com>
2025-05-05 00:01:10 +0600

    add

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from "react";
-// import { Authcontext } from "./Authprovider";
 import { NavLink, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
